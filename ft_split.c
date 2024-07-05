770aa4e757f187e9ee79cf008425e01d4d791f07
Hripsime Grigoryan <hrigrigo@c2r16s2.42yerevan.am>
2024-07-05 17:42:34 +0400

    split is finally done, without SEGV, we checked some cases and it works fine, also it does << >> <>| a<a <a a>, you can do your tests :)

--- ft_split.c
+++ ft_split.c
@@ -51,12 +51,19 @@ static int	ft_words_count(char *s, char c)
 			flag3 = 0;
 			i++;
 		}
-		if ((s[i] == c || s[i] == '\0' || s[i] == '|' || s[i] == '<' || s[i] == '>') && flag)
+		if (s[i] == c || s[i] == '\0' || s[i] == '|' || s[i] == '<' || s[i] == '>')
 		{
 			if(s[i] == '|' || s[i] == '<' || s[i] == '>')
+			{
+				if (s[i + 1] && ((s[i] == '<' && s[i + 1] == '<') || (s[i] == '>' && s[i + 1] == '>')))
+					i++;
 				count++;
-			count++;
-			flag = 0;
+			}
+			if (flag)
+			{
+				count++;
+				flag = 0;
+			}
 		}
 		else if (s[i] != c  || s[i] != '\0' || s[i] != '|' || s[i] != '<' || s[i] != '>')
 			flag = 1;
@@ -125,30 +132,38 @@ static int	fill(char **arr, char *s, char c)
 			flag2 = 0;
 			len++;
 		}
-		if ((s[len] == c || s[len] == '\0' || s[len] == '|' || s[len] == '<' || s[len] == '>') && len)
+		if ((s[len] == c || s[len] == '\0' || s[len] == '|' || s[len] == '<' || s[len] == '>'))
 		{
-			arr[i] = init(len, s);
-			if (!arr[i])
-				return (0);
-			s += len;
-			len = 0;
-			i++;
+			if (len)
+			{
+				arr[i] = init(len, s);
+				if (!arr[i])
+					return (0);
+				s += len;
+				len = 0;
+				i++;
+			}
+			else if (s[len] == c || s[len] == '\0')
+				s++;
 			if (s[len] == '|' || s[len] == '<' || s[len] == '>')
 			{
-				if (s[len] == '|' || s[len] == '<' || s[len] == '>')
+				if (s[len + 1] && ((s[len] == '<' && s[len + 1] == '<') || (s[len] == '>' && s[len + 1] == '>')))
 				{
-					arr[i] = malloc(sizeof(char) * 2);
-					if (!arr[i])
-						return (0);
-					arr[i][0] = '|';
-					arr[i][1] = 0;
-					s += len;
-					len = 0;
-					i++;
+					arr[i] = init(2, s);
+					s += 2;
 				}
+				else
+				{
+					arr[i] = init(1, s);
+					s++;
+				}
+				if (!arr[i])
+					return (0);
+				len = 0;
+				i++;
 			}
 		}
-		else if (s[len] != c && s[len] != '\0')
+		else if (s[len] != '\0' && s[len] != c )
 			len++;
 		else
 			s++;
